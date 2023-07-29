662bc041b7d4b5c990f429efe9f34a6e22cae67a
ciza99 <cizekmichal99@gmail.com>
2023-07-29 20:11:34 +0200

    feat: refactor of article table page

--- client/src/pages/articles/articles.tsx
+++ client/src/pages/articles/articles.tsx
@@ -2,136 +2,15 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getArticleList } from "api/api";
 import { Button } from "components/button";
 import { Pagination } from "components/pagination";
-import { textCell } from "components/table/cells";
-import { Column, Table } from "components/table/table";
-import { USERNAME } from "constants";
+import { Table } from "components/table/table";
 import { usePagination } from "hooks/use-pagination";
 import { usePopover } from "hooks/use-popover";
-import { Article } from "models";
-import {
-  ChangeEvent,
-  Dispatch,
-  SetStateAction,
-  useCallback,
-  useMemo,
-  useState,
-} from "react";
-import { FaPen, FaTrash } from "react-icons/fa6";
+import { useMemo, useState } from "react";
+import { FaTrash } from "react-icons/fa6";
 import { NavLink, useSearchParams } from "react-router-dom";
-import { toast } from "react-toastify";
 import { paginationQuerySchema } from "schemas";
-import { axios } from "utils/axios";
-
-const deleteArticleRequest = (articleId: string) =>
-  axios.delete(`/articles/${articleId}`);
-
-const useColumns = ({
-  checkedRows,
-  setCheckedRows,
-  rows,
-}: {
-  checkedRows: Set<number>;
-  setCheckedRows: Dispatch<SetStateAction<Set<number>>>;
-  rows: Article[];
-}) => {
-  const queryClient = useQueryClient();
-  const { mutate: deleteArticle } = useMutation(
-    (articleId: string) => deleteArticleRequest(articleId),
-    {
-      onSuccess: () => {
-        void queryClient.invalidateQueries(["articles"]);
-      },
-      onError: () => {
-        toast("Failed to delete article", { type: "error" });
-      },
-    }
-  );
-
-  const handleCheckAll = useCallback(() => {
-    setCheckedRows((prev) => {
-      if (prev.size === rows.length) {
-        return new Set();
-      }
-
-      return new Set(rows.map((_, i) => i));
-    });
-  }, [setCheckedRows, rows]);
-
-  const handleCheckChange = useCallback(
-    (rowIndex: number) => (e: ChangeEvent<HTMLInputElement>) => {
-      setCheckedRows((prev) => {
-        const newSet = new Set(prev);
-        const action = e.target.checked
-          ? (value: number) => newSet.add(value)
-          : (value: number) => newSet.delete(value);
-
-        action(rowIndex);
-        return newSet;
-      });
-    },
-    [setCheckedRows]
-  );
-
-  return useMemo<Column<Article>[]>(
-    () => [
-      {
-        key: "selection",
-        label: () => (
-          <input
-            type="checkbox"
-            onChange={handleCheckAll}
-            checked={checkedRows.size === rows.length && rows.length !== 0}
-          />
-        ),
-        render: (_, rowIndex) => (
-          <input
-            type="checkbox"
-            onChange={handleCheckChange(rowIndex)}
-            checked={checkedRows.has(rowIndex)}
-          />
-        ),
-      },
-      {
-        key: "title",
-        label: "Article title",
-        render: textCell("title"),
-      },
-      {
-        key: "perex",
-        label: "Perex",
-        render: textCell("perex"),
-      },
-      {
-        key: "author",
-        label: "Author",
-        render: () => <span>{USERNAME}</span>,
-      },
-      {
-        key: "#comments",
-        label: "# of comments",
-        render: () => <span>???</span>,
-      },
-      {
-        key: "actions",
-        label: "Actions",
-        render: (article) => (
-          <div className="flex gap-2 items-center">
-            <NavLink to={`/articles/${article.articleId}/edit`}>
-              <FaPen className="text-primary" />
-            </NavLink>
-            <Button
-              variant="icon"
-              onClick={() => deleteArticle(article.articleId!)}
-            >
-              <FaTrash className="text-red-500" />
-            </Button>
-          </div>
-        ),
-      },
-    ],
-    [deleteArticle, handleCheckChange, handleCheckAll, checkedRows, rows.length]
-  );
-};
+import { useArticleTableColumns } from "./use-article-table-columns";
+import { deleteArticleRequest } from "./api";
 
 export const Articles = () => {
   const queryClient = useQueryClient();
@@ -175,7 +54,7 @@ export const Articles = () => {
     total: articleList?.pagination?.total ?? 0,
   });
 
-  const columns = useColumns({
+  const columns = useArticleTableColumns({
     checkedRows,
     setCheckedRows,
     rows: articleList?.items ?? [],
