04514d29e59027c9540070a82a306d3e20fb24d9
ciza99 <cizekmichal99@gmail.com>
2023-07-30 13:31:00 +0200

    feat: added docker

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-export const USERNAME = import.meta.env.VITE_USERNAME as string;
+export const BASE_URL =
+  (import.meta.env.VITE_API_BASE_URL as string) ??
+  "https://fullstack.exercise.applifting.cz";
+export const USERNAME = (import.meta.env.VITE_USERNAME as string) ?? "unknown";
